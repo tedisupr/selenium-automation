27b8a08bce6dd13fb0edd2a33bbc6ba6b60f52c2
tedisupr <tedi.suprianto@gmail.com>
2023-06-02 13:57:12 +0700

    perapihan

--- saucedemo/baseLogin.py
+++ saucedemo/baseLogin.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 baseUrl = "https://www.saucedemo.com/"
